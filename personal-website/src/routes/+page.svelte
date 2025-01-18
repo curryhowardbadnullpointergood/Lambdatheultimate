69a456086cb17c14f3956be7e503c2d916f0a8cc
Ash <darkovl10@gmail.com>
2025-01-18 17:24:20 +0000

    redid the setup and added the basics core of the landing page

---
+++ personal-website/src/routes/+page.svelte
@@ -0,0 +1,30 @@
+
+
+<div> 
+  
+  <h1> Hello World </h1>
+  
+  
+  <span> I am Ash </span>
+
+  <p> Hello! I am a computer scientist that enjoys coding, soving problems and creating solutions by leveraging new technology. </p>
+    
+  <button> My Portfolio</button>
+
+  <button> Contact Me</button>
+
+
+</div>
+
+
+<!-- so there should be some animated text , i like coding, i like building projects etc blah blah on here, well that is everything for the landing page, need to wor on the nav bar need to work a bit on the button or layout etc, background css, styling, but this is basiclaly the core, shal i keep it ilke this, so tempted to, ugh that's such a computer scientist way to do things is it not? -->
+
+
+<style lang="scss">
+  
+
+  h1 {
+    color: blue;
+  }
+</style>
+
